e4dad9325d37a899508dbd541238d4a6ecc9b3b0
gksruf2848 <4455qqq@naver.com>
2022-10-06 19:24:58 +0900

    .

--- 2941_Croatia.py
+++ 2941_Croatia.py
@@ -1,4 +1,8 @@
 str = input()
 
-print(len(str) - str.count('c=') - str.count('c-') - str.count('dz=') * 2 - str.count('d-')
-    - str.count('lj') - str.count('nj') - str.count('s=') - str.count('z='))
+#print(len(str) - str.count('c=') - str.count('c-') - str.count('dz=') - str.count('d-')
+#    - str.count('lj') - str.count('nj') - str.count('s=') - str.count('z='))
+
+for i in ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']:
+    str = str.replace(i, 'a')
+print(len(str))
