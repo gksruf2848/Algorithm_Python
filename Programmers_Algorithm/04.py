c82207ad9ed7aa7a352968cb26452e55e654671e
gksruf2848 <4455qqq@naver.com>
2022-10-13 20:43:51 +0900

    .

--- Programmers_Algorithm/04.py
+++ Programmers_Algorithm/04.py
@@ -1,17 +1,23 @@
 def solution(array):
+    answer = -1
+    max_key = 0
     dic = {}
+
+    #딕셔너리 자료형에 값, 빈도를 입력해넣는다.
     for i in array:
         if i not in dic:
             dic[i] = 1
         else:
             dic[i] += 1
-        print(dic)
-    max = max(dic.values())
-    for i in range(len(dic)):
-        if dic[i] == max:
-            
-    
-    return max(dic.values())
+    max_key = max(dic.values())
+    #
+    for i in dic:
+        if dic[i] == max_key and answer != -1:
+            answer = -1
+            break
+        elif dic[i] == max_key:
+            answer = i
+    return answer
 
-arr = [1,2,3,3,4,5]
+arr = [1, 1, 2, 2]
 print(solution(arr))
