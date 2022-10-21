992c74842635beac454be3bbcbd3212d50b62783
gksruf2848 <4455qqq@naver.com>
2022-10-21 15:27:12 +0900

    .

--- Programmers_Algorithm/lv.0/120848.py
+++ Programmers_Algorithm/lv.0/120848.py
@@ -2,20 +2,10 @@ import math
 
 def solution(n):
     answer = 1
-    for i in range(2, n + 1):
+    for i in range(2, 12):
         if math.factorial(i) > n:
             answer = i - 1
             break
     return answer
 
-
-
-
-
-
-print(solution(3628799))
-print(solution(3628000))
-print(solution(720))
-print(solution(719))
-print(solution(1000))
-print(solution(3628800))
+print(solution(1))
