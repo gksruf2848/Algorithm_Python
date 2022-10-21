6c938a6bd71d1be9cf4cafec220c4d7b0462863e
gksruf2848 <4455qqq@naver.com>
2022-10-21 14:11:39 +0900

    .

--- Programmers_Algorithm/lv.0/120848.py
+++ Programmers_Algorithm/lv.0/120848.py
@@ -1,14 +1,21 @@
+import math
+
 def solution(n):
     answer = 1
-    for i in range(2, n+1):
-        print(i, answer)
-        if answer < n:
-            answer *= i
-            print(i, answer)
-        else:
-            print(i, answer)
+    for i in range(2, n + 1):
+        if math.factorial(i) > n:
             answer = i - 1
             break
     return answer
 
+
+
+
+
+
+print(solution(3628799))
+print(solution(3628000))
+print(solution(720))
+print(solution(719))
+print(solution(1000))
 print(solution(3628800))
