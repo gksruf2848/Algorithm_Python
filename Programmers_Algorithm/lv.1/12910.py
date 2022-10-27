f6672a7da755d0dbda8e1e380037cd4adebca652
gksruf2848 <4455qqq@naver.com>
2022-10-27 14:36:44 +0900

    .

--- Programmers_Algorithm/lv.1/12910.py
+++ Programmers_Algorithm/lv.1/12910.py
@@ -2,6 +2,6 @@ def solution(arr, divisor):
     answer = [i for i in arr if i%divisor == 0]
     return sorted(answer) if len(answer) != 0 else [-1]
 
-#빈 리스트를 false로 인식하기 때문에 or 연산자를 사용할 수도 있음 (대박)
+#빈 리스트를 false로 인식하기 때문에 or [-1]를 써줄 수도 있음 (대박)
 def solution(arr, divisor):
     return sorted([i for i in arr if i%divisor == 0]) or [-1]
