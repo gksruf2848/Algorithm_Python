4961a20dd14644654aa8a84e299492db75f32db8
gksruf2848 <4455qqq@naver.com>
2022-12-08 16:43:29 +0900

    .

---
+++ Programmers_Algorithm/lv.2/12951.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = ''
+    s = list(s.lower())
+    s[0] = s[0].upper()
+    
+    for i in range(1, len(s)):
+        if s[i-1] == ' ':
+            s[i] = s[i].upper()
+    return ''.join(s)
