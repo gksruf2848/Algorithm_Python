cb5687f8a7f61bd83af8a882a813568ac4a98d05
gksruf2848 <4455qqq@naver.com>
2022-12-13 20:29:03 +0900

    .

---
+++ Programmers_Algorithm/lv.2/42586.py
@@ -0,0 +1,20 @@
+import math
+
+def solution(progresses, speeds):
+    lst = []
+    answer = []
+    
+    for i in range(len(progresses)):
+        lst.append(math.ceil((100-progresses[i]) / speeds[i]))
+        
+    imsi = lst[0]
+    cnt = 1
+    for i in range(1, len(lst)):
+        if imsi < lst[i]:
+            answer.append(cnt)
+            imsi = lst[i]
+            cnt = 1
+        else:
+            cnt += 1
+    answer.append(cnt)
+    return answer
