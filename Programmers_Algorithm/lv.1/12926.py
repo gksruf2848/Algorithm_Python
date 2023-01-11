94a16972aab4888c5307320b48163f9247d0d1d6
gksruf2848 <4455qqq@naver.com>
2023-01-11 20:39:11 +0900

    .

---
+++ Programmers_Algorithm/lv.1/12926.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    answer = ''
+    low = 'abcdefghijklmnopqrstuvwxyz'
+    up = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for i in s:
+        if i in low:
+            tmp = low.index(i)
+            answer += low[(tmp+n)%26]
+        elif i in up:
+            tmp = up.index(i)
+            answer += up[(tmp+n)%26]
+        else:
+            answer += i
+    return answer
