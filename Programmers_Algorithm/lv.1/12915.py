db77f602a886d50872eb3775c65a7564078a63f6
gksruf2848 <4455qqq@naver.com>
2023-06-16 21:32:52 +0900

    .

--- Programmers_Algorithm/lv.1/12915.py
+++ Programmers_Algorithm/lv.1/12915.py
@@ -1,5 +1,3 @@
 def solution(strings, n):
-    answer = {}
-    for _ in range(len(strings)):
-        answer[strings[_][n]] = strings[_]
-    return sorted()
+    strings = sorted(sorted(strings),key=lambda x: x[n])
+    return strings
