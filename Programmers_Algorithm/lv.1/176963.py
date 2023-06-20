e3030df6d19404910c1b8653b7e1cc2d5368ee5f
gksruf2848 <4455qqq@naver.com>
2023-06-20 21:01:04 +0900

    .

--- Programmers_Algorithm/lv.1/176963.py
+++ Programmers_Algorithm/lv.1/176963.py
@@ -1,11 +1,2 @@
 def solution(name, yearning, photo):
-    answer = []
-    for i in range(len(photo)):
-        for j in range(len(photo[i])):
-            for k in range(len(name)):
-                if photo[i][j] == name[k]:
-                    photo[i][j] = yearning[k]
-            if isinstance(photo[i][j], str):
-                photo[i][j] = 0
-        answer.append(sum(photo[i]))
-    return answer
+    return [sum([yearning[name.index(j)] for j in i if j in name]) for i in photo]
